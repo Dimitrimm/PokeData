1c6fef568150e281039ecf6aa56dfcbd8435f406
Diegod064 <diegodcg064@gmail.com>
2022-08-30 23:39:14 -0300

    Card de pokemon transformado para  botão

--- poke_data/lib/pokemonButtonMap.dart
+++ poke_data/lib/pokemonButtonMap.dart
@@ -7,64 +7,68 @@ class PokemonButtonMap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-      child: Container(
-        height: 80,
-        // alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(8),
-            ),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 0,
-                  blurRadius: 4,
-                  offset: const Offset(0, 4)),
-            ]),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
-                  child: Image.asset(
-                    'assets/images/${pikomons['pokedexNumber']}.png',
-                    width: 56,
-                    height: 56,
-                  ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      pikomons['name'].toString(),
-                      style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                      textAlign: TextAlign.left,
+    return GestureDetector(
+      onTap: () {
+        // colocar o path da tela pokeInfo
+      },
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+        child: Container(
+          height: 80,
+          // alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(8),
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 0,
+                    blurRadius: 4,
+                    offset: const Offset(0, 4)),
+              ]),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                    child: Image.asset(
+                      'assets/images/${pikomons['pokedexNumber']}.png',
+                      width: 56,
+                      height: 56,
                     ),
-                    Text(pikomons['type'].toString(),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        pikomons['name'].toString(),
                         style: const TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.normal,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
                             color: Colors.black),
-                        textAlign: TextAlign.left),
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 16.0, 0.0),
-                child: Text(
-                    '#${_dexNumber(pikomons['pokedexNumber'].toString())}')),
-          ],
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(pikomons['type'].toString(),
+                          style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black),
+                          textAlign: TextAlign.left),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 16.0, 0.0),
+                  child: Text(
+                      '#${_dexNumber(pikomons['pokedexNumber'].toString())}')),
+            ],
+          ),
         ),
       ),
     );

106c9ae3bebb26812369deee60981ad77b09d428
Diogo de Souza <di_smgomes@hotmail.com>
2022-09-03 17:27:20 -0300

    Atualizando paginas de cadastro e login, e arrumando rotas

--- poke_data/lib/cadastro.dart
+++ poke_data/lib/cadastro.dart
@@ -1,118 +1,209 @@
 import 'package:flutter/material.dart';
 import 'package:poke_data/login.dart';
 
-
 void main() {
   runApp(const Cadastro());
 }
 
-class Cadastro extends StatelessWidget {
+class Cadastro extends StatefulWidget {
   const Cadastro({Key? key}) : super(key: key);
 
   @override
+  State<Cadastro> createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cadastro',
-      home: Builder(
-        builder: (context) => Scaffold(
-          body: Container(
-              padding: const EdgeInsets.only(
-                top: 40,
-                left: 40,
-                right: 40,
-              ),
-              color: Colors.white,
-              child: ListView(
+    return Scaffold(
+      body: Container(
+          color: Colors.white,
+          child: ListView(
+            children: [
+              Stack(
                 children: [
                   Center(
                     child: Text("PokeData", style: TextStyle(fontSize: 32)),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    width: 154,
-                    height: 154,
-                    child: Image.asset('images/pokebola.png'),
-                  ),
-                  const SizedBox(height: 20),
-                  Center(
-                      child: Text('Registro', style: TextStyle(fontSize: 24))),
-                  const SizedBox(height: 30),
-                  TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                        labelText: "Usuario",
-                        labelStyle: TextStyle(
-                          color: Colors.black38,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                        )),
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                          color: Colors.black38,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                        )),
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
-                        labelText: "Senha",
-                        labelStyle: TextStyle(
-                          color: Colors.black38,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                        )),
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 30),
-                  Column(
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 100,
-                        // alignment: Alignment.center,
-                        decoration: const BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20),
-                          ),
-                        ),
-                        child: SizedBox.expand(
-                          child: TextButton(
-                            onPressed: (() => Navigator.push(context, MaterialPageRoute(builder: (context) => Login()))),
-                            child: Text(
-                              'Sign up',
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                          ),
+                  IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(
+                        Icons.arrow_back_ios_outlined,
+                        size: 18.0,
+                      ))
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: 154,
+                height: 154,
+                child: Image.asset('assets/images/pokebola.png'),
+              ),
+              const SizedBox(height: 20),
+              Center(child: Text('Register', style: TextStyle(fontSize: 24))),
+              const SizedBox(height: 5),
+              userField(),
+              SizedBox(
+                height: 10,
+              ),
+              emailField(),
+              SizedBox(
+                height: 10,
+              ),
+              passwordField(),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40,
+                  right: 40,
+                ),
+                child: Text(
+                  'Choice your avatar:',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: () {},
+                      child: Container(
+                          width: 80,
+                          child: Image.asset('assets/images/jogador.png'))),
+                  TextButton(
+                      onPressed: () {},
+                      child: Container(
+                          width: 80,
+                          child: Image.asset('assets/images/jogadora.png')))
+                ],
+              ),
+              SizedBox(height: 10),
+              Column(
+                children: [
+                  Container(
+                    height: 40,
+                    width: 100,
+                    // alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: SizedBox.expand(
+                      child: TextButton(
+                        onPressed: (() => Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Login()))),
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                  const SizedBox(height: 10),
                 ],
-              )),
+              ),
+            ],
+          )),
+    );
+  }
+
+  body() {
+    return Container(
+      color: Colors.white,
+      child: ListView(children: [
+        Stack(
+          children: [
+            Center(
+              child: Text("PokeData", style: TextStyle(fontSize: 32)),
+            ),
+            IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: const Icon(
+                  Icons.arrow_back_ios_outlined,
+                  size: 18.0,
+                ))
+          ],
         ),
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          width: 154,
+          height: 154,
+          child: Image.asset('assets/images/pokebola.png'),
+        ),
+        const SizedBox(height: 20),
+        Center(child: Text('Register', style: TextStyle(fontSize: 24))),
+        const SizedBox(height: 5),
+      ]),
+    );
+  }
+
+  userField() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 40,
+        right: 40,
+      ),
+      child: TextFormField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+            labelText: "User",
+            labelStyle: TextStyle(
+              color: Colors.black38,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            )),
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  emailField() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 40,
+        right: 40,
+      ),
+      child: TextFormField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+            labelText: "Email",
+            labelStyle: TextStyle(
+              color: Colors.black38,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            )),
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  passwordField() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 40,
+        right: 40,
+      ),
+      child: TextFormField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+            labelText: "Password",
+            labelStyle: TextStyle(
+              color: Colors.black38,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            )),
+        style: const TextStyle(fontSize: 16),
       ),
     );
   }
--- poke_data/lib/login.dart
+++ poke_data/lib/login.dart
@@ -14,121 +14,139 @@ class Login extends StatefulWidget {
 }
 
 class _MyAppState extends State<Login> {
-
-  body() {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 40,
-        left: 40,
-        right: 40,
-      ),
-      child: ListView(
-        children: [
-          Center(
-            child: Text("PokeData", style: TextStyle(fontSize: 32)),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            width: 154,
-            height: 154,
-            child: Image.asset('images/pokebola.png'),
-          ),
-          const SizedBox(height: 30),
-          TextFormField(
-            autofocus: true,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-                labelText: "Email/Usuario",
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                )),
-            style: const TextStyle(fontSize: 16),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          TextFormField(
-            autofocus: true,
-            keyboardType: TextInputType.text,
-            decoration: const InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                )),
-            style: const TextStyle(fontSize: 16),
-          ),
-          const SizedBox(height: 30),
-          Column(children: [
-            Container(
-              height: 40,
-              width: 100,
-              // alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: TextButton(
-                  onPressed: (() => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Cadastro()))),
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: Text("Or", style: TextStyle(fontSize: 16)),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              width: 100,
-              height: 40,
-              decoration: const BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
-              alignment: Alignment.center,
-              child: SizedBox.expand(
-                child: TextButton(
-                    onPressed: (() => Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => TelaPrincipal(1)))),
-                    child: Text(
-                      'Sign up',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    )),
-              ),
-            ),
-          ]),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Loading',
       home: Builder(
         builder: (context) => Scaffold(
-          body: body()
-        ),
+            body: ListView(
+          children: [
+            SizedBox(height: 50),
+            body(),
+            Column(children: [
+              Container(
+                height: 40,
+                width: 100,
+                // alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: SizedBox.expand(
+                  child: TextButton(
+                    onPressed: (() => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => TelaPrincipal(1)))),
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: Text("Or", style: TextStyle(fontSize: 16)),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: 100,
+                height: 40,
+                decoration: const BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                alignment: Alignment.center,
+                child: SizedBox.expand(
+                  child: TextButton(
+                      onPressed: (() => Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Cadastro()))),
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      )),
+                ),
+              ),
+            ]),
+          ],
+        )),
       ),
+    );
+  }
+
+  userField() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 40,
+        right: 40,
+      ),
+      child: TextFormField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+            labelText: "User/Email",
+            labelStyle: TextStyle(
+              color: Colors.black38,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            )),
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  passwordField() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 40,
+        right: 40,
+      ),
+      child: TextFormField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+            labelText: "Password",
+            labelStyle: TextStyle(
+              color: Colors.black38,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+            )),
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  body() {
+    return Column(
+      children: [
+        Center(
+          child: Text("PokeData", style: TextStyle(fontSize: 32)),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          width: 154,
+          height: 154,
+          child: Image.asset('assets/images/pokebola.png'),
+        ),
+        const SizedBox(height: 30),
+        userField(),
+        SizedBox(
+          height: 10,
+        ),
+        passwordField(),
+        const SizedBox(height: 30),
+      ],
     );
   }
 }

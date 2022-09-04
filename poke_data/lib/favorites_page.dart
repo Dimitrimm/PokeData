1263c2d56b0762aea2ef49d1db21f37e598356e9
QueremHapuque <queremhapuque@WSAVANTIA173.com>
2022-09-04 10:54:34 -0300

    ajustes na tela de favoritos

--- poke_data/lib/favorites_page.dart
+++ poke_data/lib/favorites_page.dart
@@ -2,7 +2,22 @@ import 'package:flutter/material.dart';
 import './pokemonButtonMap.dart';
 import './pokemons.dart';
 
-main() => runApp(Favorite());
+main() => runApp(const FavoritesPage());
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Favorite(),
+    );
+  }
+}
 
 class Favorite extends StatefulWidget {
   const Favorite({Key? key}) : super(key: key);
@@ -16,43 +31,59 @@ class _FavoriteState extends State<Favorite> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          body: Padding(
-        padding:
-            const EdgeInsets.only(top: 42, left: 22, right: 22, bottom: 22),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Image.asset(
-              'assets/images/insignia.png',
-              width: 110,
-              height: 110,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              'Favorites',
-              style: const TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: pikomons.length,
-                itemBuilder: (context, index) {
-                  return PokemonButtonMap(pikomons[index]);
-                },
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.only(top: 42, left: 22, right: 22),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _favoritesLogo(),
+                _pokemonsListView(),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            )));
+  }
+
+  _favoritesLogo() {
+    return Stack(children: [
+      SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                "assets/images/estrela.png",
+                height: 100,
+                width: 100,
               ),
-            )
-          ],
-        ),
-      )),
+              const Text(
+                "Favoritos",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              )
+            ],
+          )),
+      IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(
+            Icons.arrow_back_ios_outlined,
+            size: 18.0,
+          ))
+    ]);
+  }
+
+  _pokemonsListView() {
+    return Expanded(
+      child: ListView.builder(
+          itemCount: pikomons.length,
+          itemBuilder: (context, index) {
+            return PokemonButtonMap(pikomons[index]);
+          }),
     );
   }
+
 }

fa5a893e807e86e1789d4176abdb30fded48968a
dimitrimm <dimitrimarinovic@gmail.com>
2022-09-23 02:04:19 -0300

    Integração Versus Page

--- poke_data/lib/versus_page.dart
+++ poke_data/lib/versus_page.dart
@@ -1,11 +1,19 @@
 import 'dart:convert';
 import 'dart:ffi';
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:http/http.dart' as http;
 
 class VersusPage extends StatefulWidget {
+<<<<<<< HEAD
+=======
+  String firstPokemonId = '742';
+  String secondPokemonId = '717';
+
+>>>>>>> efa96b7 (Integração Versus Page)
   VersusPage({Key? key}) : super(key: key);
 
   @override
@@ -13,13 +21,17 @@ class VersusPage extends StatefulWidget {
 }
 
 class _VersusPageState extends State<VersusPage> {
+<<<<<<< HEAD
   String pokemonId = '';
+=======
+>>>>>>> efa96b7 (Integração Versus Page)
   final String host = 'http://10.0.2.2:5000';
   final String endAllPokemonInfo = '/alldataofpokemon/';
   final String endAllAdvantageInfo = '/alladvantageofpokemon/';
   final String endAllWeaknessInfo = '/allweaknessofpokemon/';
   final String endAllClusterInfo = '/pokemonscluster/';
 
+<<<<<<< HEAD
   late Map allInfoMap;
   late Map allAdvantageMap;
   late List allClusterList;
@@ -29,13 +41,39 @@ class _VersusPageState extends State<VersusPage> {
   bool isInitAllAdvantageMap = false;
   bool isInitAllClusterMap = false;
   bool isInitAllWeaknessMap = false;
+=======
+  late Map firstAllInfoMap;
+  late Map firstAllAdvantageMap;
+  late List firstAllClusterList;
+  late Map firstAllWeaknessMap;
+
+  bool isInitFirstAllInfoMap = false;
+  bool isInitFirstAllAdvantageMap = false;
+  bool isInitFirstAllClusterMap = false;
+  bool isInitFirstAllWeaknessMap = false;
+
+  late Map secondAllInfoMap;
+  late Map secondAllAdvantageMap;
+  late List secondAllClusterList;
+  late Map secondAllWeaknessMap;
+
+  bool isInitSecondAllInfoMap = false;
+  bool isInitSecondAllAdvantageMap = false;
+  bool isInitSecondAllClusterMap = false;
+  bool isInitSecondAllWeaknessMap = false;
+>>>>>>> efa96b7 (Integração Versus Page)
 
   bool somtWentWrong = false;
 
   @override
   void initState() {
     super.initState();
+<<<<<<< HEAD
     fetch();
+=======
+    firstFetch();
+    secondFetch();
+>>>>>>> efa96b7 (Integração Versus Page)
   }
 
   @override
@@ -48,6 +86,7 @@ class _VersusPageState extends State<VersusPage> {
   Widget _loadingOverlay(context) {
     return const Center(child: CircularProgressIndicator());
   }
+<<<<<<< HEAD
   // Widget _body(context) {
   //   String pokemonName = allInfoMap['name'];
   //   String pokemonType = allInfoMap['genus'];
@@ -193,6 +232,52 @@ class _VersusPageState extends State<VersusPage> {
 
     double spcAtkPercent = (int.parse(spcAtk)) / 255;
     double spcDefPercent = (int.parse(spcDef)) / 255;
+=======
+
+  _body(BuildContext context) {
+    String firstPokemonName = firstAllInfoMap['name'];
+    String firstWeight = firstAllInfoMap['weight'] + " " + "KG";
+    String firstColor = firstAllInfoMap['primary_color'];
+    String firstHeight = firstAllInfoMap['height'] + 'M';
+    List<String> firstTypes = firstAllInfoMap['typing'].toString().split('~');
+
+    String firstHp = firstAllInfoMap['hp'];
+    String firstAtk = firstAllInfoMap['attack'];
+    String firstDef = firstAllInfoMap['defense'];
+    String firstSpeed = firstAllInfoMap['speed'];
+    String firstSpcAtk = firstAllInfoMap['special_attack'];
+    String firstSpcDef = firstAllInfoMap['special_defense'];
+
+    double firstHpPercent = (int.parse(firstHp)) / 255;
+    double firstAtkPercent = (int.parse(firstAtk)) / 255;
+    double firstDefPercent = (int.parse(firstDef)) / 255;
+    double firstSpeedPercent = (int.parse(firstSpeed)) / 255;
+
+    double firstSpcAtkPercent = (int.parse(firstSpcAtk)) / 255;
+    double firstSpcDefPercent = (int.parse(firstSpcDef)) / 255;
+
+    String secondPokemonName = secondAllInfoMap['name'];
+    String secondWeight = secondAllInfoMap['weight'] + " " + "KG";
+    String secondColor = secondAllInfoMap['primary_color'];
+    String secondHeight = secondAllInfoMap['height'] + 'M';
+    List<String> secondTypes = secondAllInfoMap['typing'].toString().split('~');
+
+    String secondHp = secondAllInfoMap['hp'];
+    String secondAtk = secondAllInfoMap['attack'];
+    String secondDef = secondAllInfoMap['defense'];
+    String secondSpeed = secondAllInfoMap['speed'];
+    String secondSpcAtk = secondAllInfoMap['special_attack'];
+    String secondSpcDef = secondAllInfoMap['special_defense'];
+
+    double secondHpPercent = (int.parse(secondHp)) / 255;
+    double secondAtkPercent = (int.parse(secondAtk)) / 255;
+    double secondDefPercent = (int.parse(secondDef)) / 255;
+    double secondSpeedPercent = (int.parse(secondSpeed)) / 255;
+
+    double secondSpcAtkPercent = (int.parse(secondSpcAtk)) / 255;
+    double secondSpcDefPercent = (int.parse(secondSpcDef)) / 255;
+
+>>>>>>> efa96b7 (Integração Versus Page)
     return SingleChildScrollView(
         child: Padding(
             padding:
@@ -209,6 +294,7 @@ class _VersusPageState extends State<VersusPage> {
                 const SizedBox(
                   height: 20,
                 ),
+<<<<<<< HEAD
                 ElevatedButton(onPressed: () { 
                   _firstPokemonInfo();
                   setState(() {
@@ -221,6 +307,18 @@ class _VersusPageState extends State<VersusPage> {
                 // _featuresRow(weight, color, height),
                 // _featureFieldName('Base Status'),
 
+=======
+                _firstPokemonInfo(
+                    firstTypes,
+                    firstAllWeaknessMap,
+                    firstPokemonName,
+                    firstHpPercent,
+                    firstAtkPercent,
+                    firstDefPercent,
+                    firstSpeedPercent,
+                    firstSpcAtkPercent,
+                    firstSpcDefPercent),
+>>>>>>> efa96b7 (Integração Versus Page)
                 const SizedBox(
                     height: 100,
                     child: Center(
@@ -229,8 +327,21 @@ class _VersusPageState extends State<VersusPage> {
                               color: Colors.black,
                               fontSize: 50,
                             )))),
+<<<<<<< HEAD
                 // ElevatedButton(onPressed: () => _secondPokemonInfo('3'), child: Text('child'))
                 _secondPokemonInfo('3'),
+=======
+                _secondPokemonInfo(
+                    secondTypes,
+                    secondAllWeaknessMap,
+                    secondPokemonName,
+                    secondHpPercent,
+                    secondAtkPercent,
+                    secondDefPercent,
+                    secondSpeedPercent,
+                    secondSpcAtkPercent,
+                    secondSpcDefPercent),
+>>>>>>> efa96b7 (Integração Versus Page)
               ],
             )));
   }
@@ -368,58 +479,77 @@ class _VersusPageState extends State<VersusPage> {
             "assets/images/luta.png",
           ),
         ),
-        Positioned(
-          top: 20,
-          right: 20,
-          child: Image.asset(
-            height: 45,
-            width: 35,
-            "assets/images/estrela.png",
-          ),
-        )
       ],
     );
   }
 
-  _firstPokemonInfo() {
+  _firstPokemonInfo(types, weak, name, hp, atk, def, spd, spcatk, spcdef) {
     return SizedBox(
         height: 264,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             _pokemonBasic(
-                "https://raw.githubusercontent.com/Dimitrimm/pokemonAssets/master/101.png",
-                "Eletrico",
-                "Magnético",
-                "Agua",
-                "Fogo"),
-            _pokemonStatus(),
+                "https://raw.githubusercontent.com/Dimitrimm/pokemonAssets/master/${firstAllInfoMap['id']}.png",
+                types,
+                weak),
+            _pokemonStatus(name, hp, atk, def, spd, spcatk, spcdef),
           ],
         ));
   }
 
+<<<<<<< HEAD
   _secondPokemonInfo(id) {
     pokemonId = id;
     fetchAllPokemonInfo(id);
+=======
+  _secondPokemonInfo(types, weak, name, hp, atk, def, spd, spcatk, spcdef) {
+>>>>>>> efa96b7 (Integração Versus Page)
     return SizedBox(
         height: 264,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            _pokemonStatus(),
+            _pokemonStatus(name, hp, atk, def, spd, spcatk, spcdef),
             _pokemonBasic(
-                "https://raw.githubusercontent.com/Dimitrimm/pokemonAssets/master/111.png",
-                "Poison",
-                "Flying",
-                "Redondo",
-                "Brabo"),
+                "https://raw.githubusercontent.com/Dimitrimm/pokemonAssets/master/${secondAllInfoMap['id']}.png",
+                types,
+                weak),
           ],
         ));
   }
 
+<<<<<<< HEAD
   _pokemonBasic(
       String asset, String tipo1, String tipo2, String weak1, String weak2) {
     List<String> types = allInfoMap['typing'].toString().split('~');
+=======
+  _pokemonBasic(String asset, List<String> types, weak) {
+    String type1 = weak['type1'];
+    String type2 = weak['type2'];
+    List<Widget> typeCards = <Widget>[];
+    for (String type in types) {
+      typeCards.add(Card(
+          elevation: 3,
+          color: Colors.grey[300],
+          child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+              child: Row(children: [
+                Container(
+                    padding: const EdgeInsets.only(
+                      right: 3,
+                    ),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey.shade400,
+                      backgroundImage:
+                          AssetImage('assets/images/${type}_type_icon.png'),
+                      radius: 10,
+                    )),
+                Text(type),
+              ]))));
+    }
+>>>>>>> efa96b7 (Integração Versus Page)
     return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
       Card(
         color: Colors.grey.shade300,
@@ -441,6 +571,7 @@ class _VersusPageState extends State<VersusPage> {
         ),
       ),
       Row(
+<<<<<<< HEAD
         children: [
           // Card(
           //     color: Colors.grey[300],
@@ -456,6 +587,9 @@ class _VersusPageState extends State<VersusPage> {
           //     )),
           _typeRow(types)
         ],
+=======
+        children: typeCards,
+>>>>>>> efa96b7 (Integração Versus Page)
       ),
       const SizedBox(
         height: 10,
@@ -469,6 +603,7 @@ class _VersusPageState extends State<VersusPage> {
       ),
       Row(
         children: [
+<<<<<<< HEAD
           // Card(
           //     color: Colors.grey[300],
           //     child: Padding(
@@ -482,11 +617,50 @@ class _VersusPageState extends State<VersusPage> {
           //       child: Text(weak2),
           //     )),
           _typeFeaturesRow(allWeaknessMap)
+=======
+          Card(
+              elevation: 3,
+              color: Colors.grey[300],
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 2, right: 10, top: 5),
+                  child: Row(children: [
+                    Container(
+                        padding: const EdgeInsets.only(
+                          right: 3,
+                        ),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey.shade400,
+                          backgroundImage: AssetImage(
+                              'assets/images/${type1}_type_icon.png'),
+                          radius: 7,
+                        )),
+                    Text(type1),
+                  ]))),
+          Card(
+              elevation: 3,
+              color: Colors.grey[300],
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 2, right: 10, top: 5),
+                  child: Row(children: [
+                    Container(
+                        padding: const EdgeInsets.only(
+                          right: 3,
+                        ),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey.shade400,
+                          backgroundImage: AssetImage(
+                              'assets/images/${type2}_type_icon.png'),
+                          radius: 7,
+                        )),
+                    Text(type2),
+                  ])))
+>>>>>>> efa96b7 (Integração Versus Page)
         ],
       )
     ]);
   }
 
+<<<<<<< HEAD
   _pokemonStatus() {
     String pokemonName = allInfoMap['name'];
     String pokemonType = allInfoMap['genus'];
@@ -510,18 +684,28 @@ class _VersusPageState extends State<VersusPage> {
 
     double spcAtkPercent = (int.parse(spcAtk)) / 255;
     double spcDefPercent = (int.parse(spcDef)) / 255;
+=======
+  _pokemonStatus(name, hp, atk, def, spd, spcatk, spcdef) {
+>>>>>>> efa96b7 (Integração Versus Page)
     return Column(children: [
-      _pokemonName(),
+      _pokemonName(name),
       const Text(
         "Base Status",
         style: TextStyle(fontSize: 15),
       ),
       Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+<<<<<<< HEAD
         // _percentIndicator("HP", Colors.green, 0.5),
         // _percentIndicator("ATK", Colors.blue.shade300, 0.2),
         // _percentIndicator("DEF", Colors.red.shade700, 0.8),
         // _percentIndicator("SPD", Colors.yellow, 0.1),
         _baseStatus(hpPercent, atkPercent, defPercent, speedPercent),
+=======
+        _percentIndicator("HP", Colors.green, hp),
+        _percentIndicator("ATK", Colors.blue.shade300, atk),
+        _percentIndicator("DEF", Colors.red.shade700, def),
+        _percentIndicator("SPD", Colors.yellow, spd)
+>>>>>>> efa96b7 (Integração Versus Page)
       ]),
       const SizedBox(
         height: 15,
@@ -533,25 +717,40 @@ class _VersusPageState extends State<VersusPage> {
       Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
+<<<<<<< HEAD
           // _percentIndicator("SATK", Colors.blue.shade300, 0.2),
           // _percentIndicator("SDEF", Colors.red.shade700, 0.8),
           _specialStatus(spcDefPercent, spcAtkPercent),
+=======
+          _percentIndicator("SATK", Colors.blue.shade300, spcatk),
+          _percentIndicator("SDEF", Colors.red.shade700, spcdef),
+>>>>>>> efa96b7 (Integração Versus Page)
         ],
       )
     ]);
   }
 
+<<<<<<< HEAD
   _pokemonName() {
     String pokemonName = allInfoMap['name'];
+=======
+  _pokemonName(name) {
+>>>>>>> efa96b7 (Integração Versus Page)
     return Expanded(
         child: Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Text(
+<<<<<<< HEAD
           pokemonName,
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         // Text("#01"),
+=======
+          name,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        )
+>>>>>>> efa96b7 (Integração Versus Page)
       ],
     ));
   }
@@ -567,5 +766,107 @@ class _VersusPageState extends State<VersusPage> {
       width: 100.0,
       lineHeight: 8.0,
     );
+  }
+
+  Future<Map> fetchAllPokemonInfo(id) async {
+    var response = await http.get(Uri.parse(host + endAllPokemonInfo + id));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)[0];
+    } else {
+      throw Exception("Error ao conectar-se ao servidor");
+    }
+  }
+
+  Future<Map> fetchAllWeaknessInfo(id) async {
+    var response = await http.get(Uri.parse(host + endAllWeaknessInfo + id));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)[0];
+    } else {
+      throw Exception("Error ao conectar-se ao servidor");
+    }
+  }
+
+  Future<Map> fetchAllAdvantageInfo(id) async {
+    var response = await http.get(Uri.parse(host + endAllAdvantageInfo + id));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)[0];
+    } else {
+      throw Exception("Error ao conectar-se ao servidor");
+    }
+  }
+
+  Future<List> fetchAllClusterPokemons(id) async {
+    var response = await http.get(Uri.parse(host + endAllClusterInfo + id));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception("Error ao conectar-se ao servidor");
+    }
+  }
+
+  bool checkfetch() {
+    return isInitFirstAllAdvantageMap &&
+        isInitFirstAllClusterMap &&
+        isInitFirstAllInfoMap &&
+        isInitSecondAllAdvantageMap &&
+        isInitSecondAllClusterMap &&
+        isInitSecondAllInfoMap &&
+        isInitSecondAllWeaknessMap;
+  }
+
+  firstFetch() async {
+    String id = widget.firstPokemonId;
+    try {
+      fetchAllPokemonInfo(id).then((valueInfoMap) {
+        firstAllInfoMap = valueInfoMap;
+        isInitFirstAllInfoMap = true;
+        setState(() {});
+      });
+      fetchAllAdvantageInfo(id).then((value) {
+        firstAllAdvantageMap = value;
+        isInitFirstAllAdvantageMap = true;
+        setState(() {});
+      });
+      fetchAllWeaknessInfo(id).then((value) {
+        firstAllWeaknessMap = value;
+        isInitFirstAllWeaknessMap = true;
+        setState(() {});
+      });
+      fetchAllClusterPokemons(id).then((value) {
+        firstAllClusterList = value;
+        isInitFirstAllClusterMap = true;
+        setState(() {});
+      });
+    } catch (e) {
+      somtWentWrong = true;
+    }
+  }
+
+  secondFetch() async {
+    String id = widget.secondPokemonId;
+    try {
+      fetchAllPokemonInfo(id).then((valueInfoMap) {
+        secondAllInfoMap = valueInfoMap;
+        isInitSecondAllInfoMap = true;
+        setState(() {});
+      });
+      fetchAllAdvantageInfo(id).then((value) {
+        secondAllAdvantageMap = value;
+        isInitSecondAllAdvantageMap = true;
+        setState(() {});
+      });
+      fetchAllWeaknessInfo(id).then((value) {
+        secondAllWeaknessMap = value;
+        isInitSecondAllWeaknessMap = true;
+        setState(() {});
+      });
+      fetchAllClusterPokemons(id).then((value) {
+        secondAllClusterList = value;
+        isInitSecondAllClusterMap = true;
+        setState(() {});
+      });
+    } catch (e) {
+      somtWentWrong = true;
+    }
   }
 }
